50e1931a58fea10f3042745865c61d027eb309f3
Gangadhar Mullapudi <gangadhar1373@gmail.com>
2025-05-18 20:03:38 -0400

    add functionality for form

--- client/src/components/InvestorForm.tsx
+++ client/src/components/InvestorForm.tsx
@@ -1,51 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './InvestorForm.css';
 
 const InvestorForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    dob: '',
+    phone: '',
+    streetAddress: '',
+    state: '',
+    zip: '',
+    file: null,
+  });
+  const [file, setFile] = useState<File | null>(null);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    if (!file) return alert('File is required');
+
+    const data = new FormData();
+    Object.entries(formData).forEach(([key, value]) => {
+      if (value !== null) {
+        data.append(key, value);
+      }
+    });
+    data.append('file', file);
+
+    console.log('Form data submitted:');
+    console.log(data);
   };
   return (
     <div className='investor-form-container'>
       <form onSubmit={handleSubmit}>
         <div className='form-group'>
           <label htmlFor='firstName'>First Name</label>
-          <input name='firstName' id='firstName' required />
+          <input
+            name='firstName'
+            id='firstName'
+            required
+            value={formData.firstName}
+            onChange={handleChange}
+          />
         </div>
 
         <div className='form-group'>
           <label htmlFor='lastName'>Last Name</label>
-          <input name='lastName' id='lastName' required />
+          <input
+            name='lastName'
+            id='lastName'
+            required
+            value={formData.lastName}
+            onChange={handleChange}
+          />
         </div>
 
         <div className='form-group'>
           <label htmlFor='dob'>Date of Birth</label>
-          <input type='date' name='dob' id='dob' required />
+          <input
+            type='date'
+            name='dob'
+            id='dob'
+            required
+            value={formData.dob}
+            onChange={handleChange}
+          />
         </div>
 
         <div className='form-group'>
           <label htmlFor='phone'>Phone Number</label>
-          <input type='tel' name='phone' id='phone' required />
+          <input
+            type='tel'
+            name='phone'
+            id='phone'
+            required
+            value={formData.phone}
+            onChange={handleChange}
+          />
         </div>
 
         <div className='form-group'>
-          <label htmlFor='address'>Street Address</label>
-          <input name='address' id='address' required />
+          <label htmlFor='streetAddress'>Street Address</label>
+          <input
+            name='streetAddress'
+            id='streetAddress'
+            required
+            value={formData.streetAddress}
+            onChange={handleChange}
+          />
         </div>
 
         <div className='form-group'>
           <label htmlFor='state'>State</label>
-          <input name='state' id='state' required />
+          <input
+            name='state'
+            id='state'
+            required
+            value={formData.state}
+            onChange={handleChange}
+          />
         </div>
 
         <div className='form-group'>
           <label htmlFor='zip'>ZIP Code</label>
-          <input name='zip' id='zip' required />
+          <input
+            name='zip'
+            id='zip'
+            required
+            value={formData.zip}
+            onChange={handleChange}
+          />
         </div>
 
         <div className='form-group'>
           <label htmlFor='file'>Upload File</label>
-          <input type='file' id='file' required />
+          <input
+            type='file'
+            id='file'
+            required
+            onChange={(e) => setFile(e.target.files?.[0] || null)}
+          />
         </div>
 
         <button type='submit'>Submit</button>
